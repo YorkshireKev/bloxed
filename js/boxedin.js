748e27b81a499f03678a0e338878335bb982a4de
Kev Ellis <kev@ellis-online.co.uk>
2015-05-27 21:44:17 +0100

    Working with 2 levels!

--- js/boxedin.js
+++ js/boxedin.js
@@ -5,9 +5,10 @@
 
   var stage, renderer,
     textures, map, player, startScreen,
-    left, right, up, down, enterPressed, restartPressed,
+    left, right, up, down, enterPressed,
     timeLast,
     gameState = 0,
+    level = 1,
     stats = new Stats();
   stats.setMode(0); // 0: fps, 1: ms
 
@@ -39,28 +40,31 @@
   document.body.appendChild(stats.domElement);
 
   function StartScreen() {
-    var titleText, instrText, startText, copyText;
+    var zoomIn, titleText, instrText, startText, copyText;
     titleText = new PIXI.Text("Bloxed", {
       font: "bold " + Math.floor(renderer.width / 10) + "px Verdana",
       fill: "#415df7",
       align: "center",
       stroke: "#f7ae0b",
-      strokeThickness: 6
+      strokeThickness: 35
     });
-    titleText.anchor.x = titleText.anchor.y = 0.5;
+    titleText.anchor.x = 0.5;
+    titleText.anchor.y = 0.5;
     titleText.position.x = renderer.width / 2;
-    titleText.position.y = titleText.height;
+    titleText.position.y = titleText.height / 1.5;
 
-    instrText = new PIXI.Text("Push some blocks about and collect the keys!", {
-      font: "bold " + Math.floor(renderer.width / 40) + "px Verdana",
-      fill: "#082cf0",
-      align: "center",
-      stroke: "#f7ae0b",
-      strokeThickness: 6
-    });
-    instrText.anchor.x = titleText.anchor.y = 0.5;
+    instrText = new PIXI.Text("How to play:\nPush some blocks about and collect the keys!" +
+      "\nZ - Left, X - Right, P - up and L - down... or just use the cursor Keys.", {
+        font: "bold " + Math.floor(renderer.width / 40) + "px Verdana",
+        fill: "#082cf0",
+        align: "center",
+        stroke: "#f7ae0b",
+        strokeThickness: 6
+      });
+    instrText.anchor.x = 0.5;
+    instrText.anchor.y = 0.5;
     instrText.position.x = renderer.width / 2;
-    instrText.position.y = titleText.height +  (instrText.height * 2);
+    instrText.position.y = titleText.height + (instrText.height / 1.2);
 
 
     startText = new PIXI.Text("Press ENTER to Start", {
@@ -70,9 +74,10 @@
       stroke: "#f7ae0b",
       strokeThickness: 6
     });
-    startText.anchor.x = titleText.anchor.y = 0.5;
+    startText.anchor.x = 0.5;
+    startText.anchor.y = 0.5;
     startText.position.x = renderer.width / 2;
-    startText.position.y = renderer.height / 1.7;
+    startText.position.y = renderer.height / 1.4;
 
     copyText = new PIXI.Text("(c)2015 Kev Ellis - Released under the MIT Open Source Licence", {
       font: "bold " + Math.floor(renderer.width / 40) + "px Verdana",
@@ -81,7 +86,8 @@
       stroke: "#4d4940",
       strokeThickness: 6
     });
-    copyText.anchor.x = titleText.anchor.y = 0.5;
+    copyText.anchor.x = 0.5;
+    copyText.anchor.y = 0.5;
     copyText.position.x = renderer.width / 2;
     copyText.position.y = renderer.height - copyText.height;
 
@@ -96,15 +102,30 @@
       stage.addChild(startText);
       stage.addChild(copyText);
     };
+    zoomIn = true;
+    level = 1;
 
-    StartScreen.prototype.animate = function () {
+    StartScreen.prototype.animate = function (timeElapsed) {
       if (enterPressed === true) {
         enterPressed = false;
-        map.init(); //Populate the starter map
-        player.initPlayer(8, 6);
+        map.init(1); //Populate the starter map
+        //player.initPlayer(8, 6);
         gameState = 1; //Start the game!
       }
       //Animate the game logo
+      if (zoomIn === true) {
+        startText.scale.x += timeElapsed * 0.0002;
+        startText.scale.y = startText.scale.x;
+        if (startText.scale.x > 1.05) {
+          zoomIn = false;
+        }
+      } else {
+        startText.scale.x -= timeElapsed * 0.0002;
+        startText.scale.y = startText.scale.x;
+        if (startText.scale.x < 1) {
+          zoomIn = true;
+        }
+      }
     };
   }
 
@@ -127,32 +148,80 @@
 
 
   function Map() {
-    var mapData = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9,
-                   1, 9, 9, 4, 9, 9, 9, 9, 9, 9, 2, 2, 2, 1, 9, 9, 9, 9, 9, 9,
-                   1, 9, 3, 3, 3, 3, 9, 9, 9, 0, 9, 0, 9, 1, 9, 9, 9, 9, 9, 9,
-                   1, 9, 3, 9, 9, 9, 9, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
-                   1, 9, 3, 9, 0, 9, 9, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
-                   1, 8, 3, 9, 9, 0, 9, 9, 9, 9, 2, 8, 2, 1, 9, 9, 9, 9, 9, 9,
-                   1, 1, 1, 9, 0, 9, 0, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
-                   9, 9, 1, 9, 9, 0, 9, 0, 9, 9, 2, 6, 2, 1, 1, 9, 9, 9, 9, 9,
-                   9, 9, 1, 9, 0, 9, 0, 9, 9, 9, 2, 6, 2, 9, 1, 1, 1, 1, 1, 1,
-                   9, 9, 1, 0, 9, 0, 9, 0, 9, 9, 2, 6, 2, 9, 9, 9, 0, 9, 8, 1,
-                   9, 9, 1, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0, 9, 9, 9, 9, 1,
-                   9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    var level1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9,
+                  1, 9, 9, 4, 9, 9, 9, 9, 8, 9, 2, 2, 2, 1, 9, 9, 9, 9, 9, 9,
+                  1, 9, 3, 3, 3, 3, 9, 9, 9, 0, 9, 0, 9, 1, 9, 9, 9, 9, 9, 9,
+                  1, 9, 3, 9, 9, 9, 9, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
+                  1, 9, 3, 9, 0, 9, 9, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
+                  1, 9, 3, 9, 9, 0, 9, 9, 9, 9, 2, 9, 2, 1, 9, 9, 9, 9, 9, 9,
+                  1, 1, 1, 9, 0, 9, 0, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
+                  9, 9, 1, 9, 9, 0, 9, 0, 9, 9, 2, 6, 2, 1, 1, 9, 9, 9, 9, 9,
+                  9, 9, 1, 9, 0, 9, 0, 9, 9, 9, 2, 6, 2, 9, 1, 1, 1, 1, 1, 1,
+                  9, 9, 1, 0, 9, 0, 9, 0, 9, 9, 2, 6, 2, 9, 9, 9, 0, 9, 9, 1,
+                  9, 9, 1, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0, 9, 9, 9, 9, 1,
+                  9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      level2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9, 9, 9,
+                1, 9, 9, 4, 9, 9, 9, 9, 9, 9, 2, 2, 2, 1, 9, 9, 9, 9, 9, 9,
+                1, 9, 3, 3, 3, 3, 9, 9, 9, 0, 9, 0, 9, 1, 9, 9, 9, 9, 9, 9,
+                1, 9, 3, 9, 9, 9, 9, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
+                1, 9, 3, 9, 0, 9, 9, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
+                1, 8, 3, 9, 9, 0, 9, 9, 9, 9, 2, 8, 2, 1, 9, 9, 9, 9, 9, 9,
+                1, 1, 1, 9, 0, 9, 0, 9, 9, 9, 2, 6, 2, 1, 9, 9, 9, 9, 9, 9,
+                9, 9, 1, 9, 9, 0, 9, 0, 9, 9, 2, 6, 2, 1, 1, 9, 9, 9, 9, 9,
+                9, 9, 1, 9, 0, 9, 0, 9, 9, 9, 2, 6, 2, 9, 1, 1, 1, 1, 1, 1,
+                9, 9, 1, 0, 9, 0, 9, 0, 9, 9, 2, 6, 2, 9, 9, 9, 0, 9, 8, 1,
+                9, 9, 1, 9, 0, 9, 9, 9, 9, 9, 9, 9, 9, 9, 0, 9, 9, 9, 9, 1,
+                9, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      mapData = [],
       spriteToMap = [],
       WIDTH = 20,
       HEIGHT = 12,
       mapSprite = [],
-      keyCount = 0,
+      keyCount,
       keyBounceDir = 0,
-      keyScale = 0.8;
-    Map.prototype.init = function () {
+      keyScale = 0.8,
+
+      LevelCompleteText = new PIXI.Text("Level Complete!", {
+        font: "bold " + Math.floor(renderer.width / 20) + "px Verdana",
+        fill: "#ff2700",
+        align: "center",
+        stroke: "#f7ae0b",
+        strokeThickness: 6
+      });
+    LevelCompleteText.anchor.x = 0.5;
+    LevelCompleteText.anchor.y = 0.5;
+    LevelCompleteText.position.x = renderer.width / 2;
+    LevelCompleteText.position.y = renderer.height / 2;
+
+    Map.prototype.init = function (level) {
       var ix = 0,
-        iy = 0;
+        iy = 0,
+        px,
+        py;
+      
+      //Load the appropriate level into mapData;
+      switch (level) {
+      case 1:
+        mapData = level1.slice();
+        px = 8; //Player x start pos
+        py = 6; //Player y start pos
+        break;
+      case 2:
+        mapData = level2.slice();
+        px = 8;
+        py = 6;
+        break;
+      default:
+        //No more levels, so tell player they've completed all of the levels!
+        gameState = 4;
+        return;
+      }
+
       //First remove any existing objects from stage
       while (stage.children[0]) {
         stage.removeChild(stage.children[0]);
       }
+      keyCount = 0;
       //Loop through mapData and add a sprite for each non-blank (aka no 9) tile.
       for (ix = 0; ix < (HEIGHT * WIDTH); ix += 1) {
         if (mapData[ix] !== 9) {
@@ -172,6 +241,7 @@
           }
         }
       }
+      player.initPlayer(px, py); //Reset the players position.
     };
     Map.prototype.moveTile = function (x, y, timeElapsed, speed, direction, isMoving) {
       var spriteIx,
@@ -259,11 +329,23 @@
       keyCount -= 1;
       mapData[ix] = 9;
       mapSprite[spriteIx].visible = false;
-      window.console.log("Key collected. " + keyCount + " keys left to collect.");
       if (keyCount === 0) {
         //All keys collected. Level Complete!
-        //TODO - LEVEL COMPLETE
-        window.console.log("LEVEL COMPLETE!");
+        gameState = 2;
+        LevelCompleteText.scale.x = 1;
+        LevelCompleteText.scale.x = 1;
+        stage.addChild(LevelCompleteText);
+        //LevelCompleteText.visibility = true;
+      }
+    };
+
+    Map.prototype.complete = function (timeElapsed) {
+      LevelCompleteText.scale.x += timeElapsed * 0.0004;
+      LevelCompleteText.scale.y = LevelCompleteText.scale.x;
+      if (LevelCompleteText.scale.x > 3) {
+        level += 1;
+        map.init(level);
+        gameState = 1;
       }
     };
   }
@@ -281,6 +363,8 @@
     playerSprite.anchor.y = 0.5;
 
     Player.prototype.initPlayer = function (x, y) {
+      isMoving = false;
+      direction = 2;
       playerSprite.gridX = x;
       playerSprite.gridY = y;
       playerSprite.x = (playerSprite.gridX * 64) - 32;
@@ -469,8 +553,10 @@
       break;
     case 82:
     case 27:
-      //Key: Enter/Return
-      restartPressed = true;
+      //Key: Escape/R
+      if (gameState === 1) {
+        map.init(level);
+      }
       break;
     }
   }, false);
@@ -515,7 +601,7 @@
     stats.begin();
     switch (gameState) {
     case 0: //Start Screen
-      startScreen.animate();
+      startScreen.animate(timeElapsed);
       break;
 
     case 1: //Game
@@ -524,6 +610,8 @@
       break;
 
     case 2: //Level Complete
+      player.move(timeElapsed);
+      map.complete(timeElapsed);
       break;
 
     case 3: //All Levels Complete - Game Over
