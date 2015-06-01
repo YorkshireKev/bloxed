41913e3de8d7dae855c1949f5a7b2d72bd6ee51c
Kev Ellis <kev@ellis-online.co.uk>
2015-06-01 21:23:31 +0100

    Add description to top of code

--- js/bloxed.js
+++ js/bloxed.js
@@ -1,5 +1,48 @@
 /*jslint browser: true*/
 /*global PIXI, levels*/
+
+/*
+---------------------------------------------------------------------------
+
+BLOXED
+======
+
+Written By Kevin Ellis May 2015.
+
+A simple 2D "Push the blocks and collect the keys" puzzle game.
+This game uses the excellent Pixi.js WebGL library and was mainly written
+as a learning exercise for Pixi.js and JavaScript techniques. As a result
+my code may not be the most robust or eloquent, but it might be of some use
+to others starting out with 2D browser based game programming.
+
+---------------------------------------------------------------------------
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Kevin Ellis
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---------------------------------------------------------------------------
+
+*/
+
 (function () {
   "use strict";
 
